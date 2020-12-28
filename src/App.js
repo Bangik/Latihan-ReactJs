e0a72c9c8c66b8fdfaa80da84bcb6feb062854c5
Bangik <47439247+Bangik@users.noreply.github.com>
2020-12-28 19:56:05 +0700

    nambahkan  paper dan prop types

--- src/App.js
+++ src/App.js
@@ -1,57 +1,56 @@
 import React from "react";
 import "./styles.css";
+import Paper from "./components/Paper";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button name="button" className="header-btn">
+    <Paper>
+      <section className="header">
+        <button name="button" className="header-btn">
+          Add
+        </button>
+        <h1 className="header-title">Todo List</h1>
+        <button name="button" className="header-btn main-red-color">
+          Clear
+        </button>
+      </section>
+      <section className="add">
+        <form className="add-form">
+          <input type="text" name="" value="" className="add-input"></input>
+          <button type="button" name="button" className="add-btn">
             Add
           </button>
-          <h1 className="header-title">Todo List</h1>
-          <button name="button" className="header-btn main-red-color">
-            Clear
-          </button>
-        </section>
-        <section className="add">
-          <form className="add-form">
-            <input type="text" name="" value="" className="add-input"></input>
-            <button type="button" name="button" className="add-btn">
-              Add
-            </button>
-          </form>
-        </section>
-        <section className="todos">
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">todododo</span>
-          </div>
-        </section>
-      </div>
-    </div>
+        </form>
+      </section>
+      <section className="todos">
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">todododo</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
