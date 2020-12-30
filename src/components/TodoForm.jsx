b106f95527624e9bb7e7c3c2e11fb45ed59c1055
Bangik <47439247+Bangik@users.noreply.github.com>
2020-12-30 19:56:27 +0700

    form line 7

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,7 +4,7 @@ const TodoForm = () => {
   return (
     <section className="add">
       <form className="add-form">
-        <input type="text" name="" value="" className="add-input"></input>
+        <input type="text" name="" className="add-input"></input>
         <button type="button" name="button" className="add-btn">
           Add
         </button>
